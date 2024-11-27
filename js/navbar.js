f85e575f4a775791e7116e440e09f8b4976bd336
rbubarb <jonatanhuban@gmail.com>
2024-11-27 16:20:22 +0100

    Added js folder and navbar.js

---
+++ js/navbar.js
@@ -0,0 +1,47 @@
+let link_text_table = {
+    "index": "Főoldal",
+    "kavek": "Kávéfajták",
+    "suti": "Sütemények",
+    "blog": "Blog"
+}
+
+document.body.onload = makeNavbar(["index", "kavek", "logo", "suti", "blog"]);
+
+function makeNavbar(args) {
+    let navbar = document.createElement("div");
+
+    navbar.className = "navbar";
+    
+    let nav_logic = document.createElement("div");
+
+    nav_logic.className = "logic";
+
+    args.forEach(element => {
+        if (element != "logo") {
+            let menuopt = document.createElement("a");
+            menuopt.href = element + ".html";
+            menuopt.className = "navlink";
+
+            let opt_txt = document.createElement("p");
+            let txtnode = document.createTextNode(link_text_table[element]);
+            opt_txt.appendChild(txtnode);
+
+            menuopt.appendChild(opt_txt);
+            nav_logic.appendChild(menuopt);
+        }
+        else {
+            let menulogo = document.createElement("div");
+            menulogo.className = "logo";
+
+            let logo_img = document.createElement("img");
+            logo_img.src = "pics/logo.png"
+
+            menulogo.appendChild(logo_img);
+            nav_logic.appendChild(menulogo);
+        }
+    });
+    
+    navbar.appendChild(nav_logic);
+
+    document.body.appendChild(navbar);
+}
